c3f7018545b8fdb5bc8b03c3cdbe3bcb858182cc
hbtne <144417841+hbtne@users.noreply.github.com>
2024-09-19 23:06:19 +0700

    Update FirstForm.Designer.cs

--- Bai1/Bai1/Presentation/FirstForm.Designer.cs
+++ Bai1/Bai1/Presentation/FirstForm.Designer.cs
@@ -1,10 +1,11 @@
-﻿namespace Bai1.Presentation
+namespace Bai1.Presentation
 {
     partial class FirstForm
     {
-        
+      
         private System.ComponentModel.IContainer components = null;
 
+       
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -14,6 +15,7 @@
             base.Dispose(disposing);
         }
 
+        
         private void InitializeComponent()
         {
             label1 = new Label();
@@ -35,8 +37,8 @@
             label1.Name = "label1";
             label1.Size = new Size(286, 25);
             label1.TabIndex = 1;
-            label1.Text = "Nhập số lượng vật nuôi";
-          
+            label1.Text = "Nhập số lượng vật nuôi ban đầu";
+           
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);
             label2.Location = new Point(76, 53);
@@ -44,15 +46,67 @@
             label2.Size = new Size(77, 15);
             label2.TabIndex = 2;
             label2.Text = "Số lượng Bò:";
-         
+           
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);trại";
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);
+            label3.Location = new Point(68, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(85, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Số lượng Cừu:";
+            
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);
+            label4.Location = new Point(76, 128);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Số lượng Dê:";
+            
+            bt_confirm.Location = new Point(102, 165);
+            bt_confirm.Name = "bt_confirm";
+            bt_confirm.Size = new Size(109, 35);
+            bt_confirm.TabIndex = 3;
+            bt_confirm.Text = "Xác nhận";
+            bt_confirm.UseVisualStyleBackColor = true;
+            bt_confirm.Click += bt_confirm_Click;
+
+            nud_cowAmount.Location = new Point(159, 51);
+            nud_cowAmount.Name = "nud_cowAmount";
+            nud_cowAmount.Size = new Size(70, 23);
+            nud_cowAmount.TabIndex = 4;
+            
+            nud_sheepAmount.Location = new Point(159, 88);
+            nud_sheepAmount.Name = "nud_sheepAmount";
+            nud_sheepAmount.Size = new Size(70, 23);
+            nud_sheepAmount.TabIndex = 4;
+           
+            nud_goatAmount.Location = new Point(159, 126);
+            nud_goatAmount.Name = "nud_goatAmount";
+            nud_goatAmount.Size = new Size(70, 23);
+            nud_goatAmount.TabIndex = 4;
+           
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(308, 212);
+            Controls.Add(nud_goatAmount);
+            Controls.Add(nud_sheepAmount);
+            Controls.Add(nud_cowAmount);
+            Controls.Add(bt_confirm);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FirstForm";
+            Text = "Quản lý nông trại";
             ((System.ComponentModel.ISupportInitialize)nud_cowAmount).EndInit();
             ((System.ComponentModel.ISupportInitialize)nud_sheepAmount).EndInit();
             ((System.ComponentModel.ISupportInitialize)nud_goatAmount).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
+
+       
 
         private Label label1;
         private Label label2;
